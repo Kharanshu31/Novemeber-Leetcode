ac49fde021d10963e471f660ca239fba0ad9d815
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-11-23 18:34:48 +0530

    Day 23

--- House Robber III.cpp
+++ House Robber III.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     map<TreeNode*,int>m;
@@ -35,6 +24,5 @@ public:
         int res=max(root->val+sum,newsum);
         m[root]=res;
         return res;
-        
     }
 };
